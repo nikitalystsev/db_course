fc0aa6308ead7128ad8acb0d164b7ee999b6e939
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-08-29 19:15:50 +0300

    реализовал лучше

--- src/internal/repositories/impl/migrations/fill_db/000005_create_funcs.down.sql
+++ src/internal/repositories/impl/migrations/fill_db/000005_create_funcs.down.sql
@@ -1,3 +1,5 @@
-drop trigger if exists check_avg_rating on ss.rating;
+drop trigger if exists delete_sale_product_avg_rating on ss.rating;
+drop trigger if exists insert_sale_product_avg_rating on ss.rating;
 
-drop function if exists ss.update_avg_rating;
+drop function if exists ss.update_sale_product_avg_rating;
+drop function if exists ss.get_sale_product_avg_rating;
--- src/internal/repositories/impl/migrations/fill_db/000005_create_funcs.up.sql
+++ src/internal/repositories/impl/migrations/fill_db/000005_create_funcs.up.sql
@@ -1,26 +1,63 @@
-create or replace function ss.update_avg_rating()
-    returns trigger
+create or replace function ss.get_sale_product_avg_rating(_sale_product_id uuid)
+    returns numeric
 as
 $$
 declare
-    new_avg_rating numeric;
+    cnt_ratings int;
+    avg_rating  numeric;
 
 begin
-    select avg(rating)
-    into new_avg_rating
-    from ss.rating
-    where sale_product_id = new.sale_product_id;
+    select count(*)
+    into cnt_ratings
+    from ss.rating as r
+    where r.sale_product_id = _sale_product_id;
 
-    update ss.sale_product set avg_rating = new_avg_rating where id = new.sale_product_id;
+    if cnt_ratings != 0 then
+        select avg(r.rating)
+        into avg_rating
+        from ss.rating as r
+        where r.sale_product_id = _sale_product_id;
 
-    return null;
+        return avg_rating;
+    else
+        return null;
+    end if;
 end;
 $$
     language plpgsql;
 
+create or replace function ss.update_sale_product_avg_rating()
+    returns trigger
+as
+$$
+begin
+    if tg_op = 'INSERT' then
 
-create or replace trigger check_avg_rating
+        update ss.sale_product
+        set avg_rating = ss.get_sale_product_avg_rating(new.sale_product_id)
+        where id = new.sale_product_id;
+
+        return new;
+    elseif tg_op = 'DELETE' then
+
+        update ss.sale_product
+        set avg_rating = ss.get_sale_product_avg_rating(old.sale_product_id)
+        where id = old.sale_product_id;
+
+        return old;
+    end if;
+end;
+$$
+    language plpgsql;
+
+create or replace trigger insert_sale_product_avg_rating
     after insert
     on ss.rating
     for each row
-execute function ss.update_avg_rating();
+execute function ss.update_sale_product_avg_rating();
+
+create or replace trigger delete_sale_product_avg_rating
+    after delete
+    on ss.rating
+    for each row
+execute function ss.update_sale_product_avg_rating();
