903a21fd7da02ec3b6a85bfd99710a76560a85be
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-08-31 21:59:37 +0300

    small fix

--- src/internal/repositories/testing/tests/trigger_testing.sql
+++ src/internal/repositories/testing/tests/trigger_testing.sql
@@ -1,6 +1,6 @@
 begin;
 
-select plan(1);
+select plan(2);
 
 insert into ss.rating (user_id, sale_product_id, review, rating)
 
@@ -9,6 +9,14 @@ values ('362b79f6-d671-404a-b1a0-5a655aebc1b6', '53b7ff0a-b5cd-4107-85af-1617b13
 select is(
                ss.get_sale_product_avg_rating('53b7ff0a-b5cd-4107-85af-1617b137aa23'),
                5.0
+       );
+
+delete from ss.rating
+where user_id = '362b79f6-d671-404a-b1a0-5a655aebc1b6' and sale_product_id = '53b7ff0a-b5cd-4107-85af-1617b137aa23';
+
+select is(
+               ss.get_sale_product_avg_rating('53b7ff0a-b5cd-4107-85af-1617b137aa23'),
+               null
        );
 
 select *
